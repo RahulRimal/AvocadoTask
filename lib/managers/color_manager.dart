56ddd9df2114e8a77c0c2a6b29598c471b3a6e99
Rahul Rimal <genuinetechnology9@gmail.com>
2022-06-09 05:57:42 +0545

    Working on order list card

---
+++ lib/managers/color_manager.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorManager
+{
+  static Color primary = Color.fromRGBO(49, 198, 169, 1);
+  static Color secondary = Color.fromRGBO(60,165,159, 1);
+
+  static Color containerBackground = Color.fromRGBO(241,251,249, 1);
+  static Color listTileColor = Color.fromRGBO(236,247,251, 1);
+}
--- lib/screens/list_of_order_screen.dart
+++ lib/screens/list_of_order_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../managers/color_manager.dart';
+
 class ListOfOrderScreen extends StatelessWidget {
   const ListOfOrderScreen({Key? key}) : super(key: key);
 
@@ -7,11 +9,160 @@ class ListOfOrderScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('List of Orders'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {},
+        ),
+        title: Center(child: Text('Completed order')),
       ),
-      body: Center(
-        child: Text('List of Orders'),
+      body: Card(
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10,),
+        color: ColorManager.secondary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        elevation: 3,
+        shadowColor: Colors.grey,
+        // margin: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                'Order',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                color: ColorManager.containerBackground,
+                // borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(children: [
+                Container(
+              height: 60,
+              child: Center(
+                child: Text(
+                  'Jenny Yohana',
+                  
+                  style: TextStyle(
+                    color: ColorManager.primary,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Colors.grey,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Icon(
+                          Icons.watch_later_outlined,
+                          color: ColorManager.secondary,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          '10:30',
+                          
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Icon(
+                          Icons.calendar_today_outlined,
+                          color: ColorManager.secondary,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          '06 May 2021',
+                          
+                          
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Prescription consultation',
+                      
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                    width: 30,
+                    // height: 30,
+                    child: CheckboxListTile(
+                      tileColor: ColorManager.secondary,
+                      checkColor: Colors.white,
+                      activeColor: ColorManager.secondary,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      value: true,
+                      onChanged: (value) {},
+                    )),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Divider(
+              thickness: 1,
+              color: Colors.grey,
+            ),
+            Container(
+              height: 60,
+              decoration: BoxDecoration(
+                  // color: ColorManager.containerBackground,
+                  ),
+              child: Center(
+                child: Text(
+                  'Finished',
+                  style: TextStyle(
+                    color: ColorManager.primary,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+              ],),
+            ),
+          ],
+        ),
       ),
+      
     );
   }
 }
